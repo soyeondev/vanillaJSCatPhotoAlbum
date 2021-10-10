f9ae1cbce8715cad4a8e719c997b6fd169c9907e
김소연 <soyeon@gimsoyeon-ui-MacBookPro.local>
2021-10-10 21:32:19 +0900

    inital commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import DarkMode from './components/ChangeMode.js';
+
+export default class App {
+    constructor($target){
+        const darkMode = new DarkMode($target);
+        
+    }
+
+}
---
+++ src/components/ChangeMode.js
@@ -0,0 +1,41 @@
+export default class DarkMode {
+    constructor($target){
+        const darkModeWrapper = document.createElement("div");
+        darkModeWrapper.className = "dark-mode-wrapper";
+        this.darkModeWrapper = darkModeWrapper;
+        this.currentMode = window.matchMedia("(perfers-color-scheme: dark)")
+            .matches
+            ? "dark"
+            : "light";
+        $target.appendChild(darkModeWrapper);
+        this.render();
+    }
+    
+    toggleMode() {
+        const body = document.querySelector("body");
+        if(this.currentMode == "dark"){
+            this.currentMode = "light";
+            body.className = "light";
+        } else {
+            this.currentMode = "dark";
+            body.className = "dark";
+        }
+    }
+
+    render(){
+        const darkModeBtn = document.createElement("span");
+        darkModeBtn.className = "dark-mode-btn";
+        const body = document.querySelector("body");
+        if(this.currentMode == "dark"){
+            body.className = "dark";
+        } else {
+            body.className = "light";
+        }
+        darkModeBtn.innerText = this.currentMode == "dark" ? "🌑" : "🌕";
+        darkModeBtn.addEventListener("click", (e) => {
+            this.toggleMode();
+        });
+        this.darkModeWrapper.appendChild(darkModeBtn);
+    }
+
+}
